520b17409568b7bd245867f96051052f8adf1f79
negtrooper1 <negtrooper0@yahoo.com>
2014-03-18 19:39:04 -0400

    initial commit

---
+++ Runovation/DataViewController.h
@@ -0,0 +1,16 @@
+//
+//  DataViewController.h
+//  Runovation
+//
+//  Created by Devon Wells on 3/18/14.
+//  Copyright (c) 2014 Runovation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
---
+++ Runovation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Runovation
+//
+//  Created by Devon Wells on 3/18/14.
+//  Copyright (c) 2014 Runovation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
