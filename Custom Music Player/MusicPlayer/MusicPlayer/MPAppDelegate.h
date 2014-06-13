498229654eebd1d5aa0c605303f7fafa9eac1246
Eli Stine <elistine@Eli-Stines-MacBook-Pro.local>
2014-06-13 12:29:40 -0400

    Added MusicPlayer project (after creating it with tutorial) and webpage link about creating outlets

---
+++ Custom Music Player/MusicPlayer/MusicPlayer/MPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MPAppDelegate.h
+//  MusicPlayer
+//
+//  Created by dev27 on 5/13/13.
+//  Copyright (c) 2013 Codigator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Custom Music Player/MusicPlayer/MusicPlayer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MusicPlayer
+//
+//  Created by dev27 on 5/13/13.
+//  Copyright (c) 2013 Codigator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([MPAppDelegate class]));
+  }
+}
